6366d72647488ce96944d82a0cdcb83d910c93fe
kuni4210 <kuni4210@naver.com>
2025-02-21 17:07:13 +0900

    config 수정

--- config/config.py
+++ config/config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
 
+if not os.getenv("APP_ENV"):
+    load_dotenv()
 
 # Kafka 설정
-KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL", "localhost:9092")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "ticketing-reservation")
-KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID", "reservation-group")
+KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID")
