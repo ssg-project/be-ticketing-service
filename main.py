522318dd0cc867f2126750631c2321690e781612
simsangho6448 <mjj00travel@gmail.com>
2025-02-28 12:33:54 +0900

    basicConfig에서 getLogger로 변경

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ from prometheus_fastapi_instrumentator import Instrumentator
 import uvicorn
 import logging
 
+# 로거 생성
+logger = logging.getLogger("ticketing-service")
+logger.setLevel(logging.INFO)
+
+
 # 로깅 설정
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - ticketing-service - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()]
+# logging.basicConfig(
+#     level=logging.INFO,
+#     format="%(asctime)s - ticketing-service - %(name)s - %(levelname)s - %(message)s",
+#     handlers=[logging.StreamHandler()]
+# )
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    "%(asctime)s - ticketing-service - %(name)s - %(levelname)s - %(message)s"
 )
+handler.setFormatter(formatter)
+
+# 기존 핸들러 제거 후 새로운 핸들러 추가 (중복 방지)
+if not logger.hasHandlers():
+    logger.addHandler(handler)
+
 
 app = FastAPI()
 
