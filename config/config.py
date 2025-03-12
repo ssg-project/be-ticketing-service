1e5e31397dff9a1b575f044cd948fdd40bb02096
myeongsoyi <myeongsoyi@gmail.com>
2025-03-12 14:43:54 +0900

    feat: config 변경

--- config/config.py
+++ config/config.py
@@ -1,10 +1,38 @@
 import os
 from dotenv import load_dotenv
+import boto3
+import json
+from botocore.exceptions import ClientError
 
 if not os.getenv("APP_ENV"):
     load_dotenv()
 
-# Kafka 설정
-KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
-KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID")
+    # Kafka 설정
+    KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
+    KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+    KAFKA_GROUP_ID = os.getenv("KAFKA_GROUP_ID")
+
+else:
+    secret_name = "secret/ticketing/fe"
+    region_name = "ap-northeast-2"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    
+    secret_data = json.loads(secret)
+
+    KAFKA_BROKER_URL = secret_data["KAFKA_BROKER_URL"]
+    KAFKA_TOPIC = secret_data["KAFKA_TOPIC"]
+    KAFKA_GROUP_ID = secret_data["KAFKA_GROUP_ID"]
